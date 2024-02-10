19b128f37a0fc1f200c499e0d31c09deb84dd141
Maria Sophia M. Solo <solomariasophia@gmail.com>
2024-02-10 17:17:14 +0800

    entry

--- spelling_checker.py
+++ spelling_checker.py
@@ -6,7 +6,7 @@
 # make sure first that you have installed the textblob module
 
 # import tkinter and the textblob
-import tkinter as tk
+import tkinter
 from tkinter import *
 from textblob import TextBlob
 
@@ -17,14 +17,15 @@ window.minsize(width=500, height=500)
 window.config(background="black")
 window.resizable(False,False)
 
-image = tk.PhotoImage (file="spelling.jpg")
-image_label = tk.Label (picture = image, borderwidth=0)
-image_label.place (x=0, y=0)
 # heading
 heading = Label(window, text="Spelling Checker", font=("Impact", 30,), fg="pink", bg="black")
 heading.place (x=100, y= 90)
 
 # entry
+enter_text = Entry (window, justify="center", width=20, font=("Glacial Indifference", 25), fg="pink", bg="black", border=2)
+enter_text.place(x=65, y= 150)
+enter_text.focus()
+
 # button
 # command
 
