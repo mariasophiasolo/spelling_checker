5728e54b3d557de77b3f13211bbc3d9f81a19f1e
Maria Sophia M. Solo <solomariasophia@gmail.com>
2024-02-10 15:39:49 +0800

    main window

---
+++ spelling_checker.py
@@ -0,0 +1,24 @@
+# pseudocode
+
+# look for any exsisting python program on youtube
+# re-create that project then send a demo that it is working
+
+# make sure first that you have installed the textblob module
+
+# import tkinter and the textblob
+import tkinter
+from tkinter import *
+from textblob import TextBlob
+
+# create the main window for your program
+window = Tk()
+window.title("Spelling Checker")
+window.geometry("500x400")
+window.config(background="pink")
+
+# heading
+# entry
+# button
+# command
+
+window.mainloop()
